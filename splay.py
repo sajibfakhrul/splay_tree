11f93a74b78cc3de50a26675326843bb9145a85e
sajibfakhrul <54445594+sajibfakhrul@users.noreply.github.com>
2024-04-12 10:15:18 -0700

    Update splay.py

--- splay.py
+++ splay.py
@@ -1,111 +1,241 @@
-#Python Code for Insertion Operation of Splay Trees
+import sys
+
 class Node:
-	def __init__(self, key):
-		self.key = key
-		self.left = None
-		self.right = None
-		
-def newNode(key): 
-	return Node(key) 
-	
-def rightRotate(x): 
-	y = x.left 
-	x.left = y.right 
-	y.right = x
-	return y
+    def  __init__(self, data):
+        self.data = data
+        self.parent = None
+        self.left = None
+        self.right = None
 
-def leftRotate(x): 
-	y = x.right 
-	x.right = y.left 
-	y.left = x
-	return y
+class SplayTree:
+    def __init__(self):
+        self.root = None
 
-def splay(root, key): 
-	# Base cases: root is None or key is present at root 
-	if root == None or root.key == key: 
-		return root 
-	# Key lies in left subtree 
-	if root.key > key: 
-		# Key is not in tree, we are done 
-		if root.left == None: 
-			return root 
-		# Zig-Zig (Left Left) 
-		if root.left.key > key: 
-			# First recursively bring the key as root of left-left 
-			root.left.left = splay(root.left.left, key) 
-			# Do first rotation for root, second rotation is done after else 
-			root = rightRotate(root) 
-		elif root.left.key < key: # Zig-Zag (Left Right) 
-			# First recursively bring the key as root of left-right 
-			root.left.right = splay(root.left.right, key) 
-			# Do first rotation for root.left 
-			if root.left.right != None: 
-				root.left = leftRotate(root.left) 
-		# Do second rotation for root 
-		return root if root.left == None else rightRotate(root) 
-	else: # Key lies in right subtree 
-		# Key is not in tree, we are done 
-		if root.right == None: 
-			return root 
-		# Zig-Zag (Right Left) 
-		if root.right.key > key: 
-			# Bring the key as root of right-left 
-			root.right.left = splay(root.right.left, key) 
-			# Do first rotation for root.right 
-			if root.right.left != None: 
-				root.right = rightRotate(root.right) 
-		elif root.right.key < key: # Zag-Zag (Right Right) 
-			# Bring the key as root of right-right and do first rotation 
-			root.right.right = splay(root.right.right, key) 
-			root = leftRotate(root) 
-		# Do second rotation for root 
-		return root if root.right == None else leftRotate(root) 
+    def __print_helper(self, currPtr, indent, last):
+        # print the tree structure on the screen
+        if currPtr != None:
+            sys.stdout.write(indent)
+            if last:
+                sys.stdout.write("R----")
+                indent += "     "
+            else:
+                sys.stdout.write("L----")
+                indent += "|    "
 
-# Function to insert a new key k in splay tree with given root 
-def insert(root, k): 
-	# Simple Case: If tree is empty 
-	if (root == None): 
-		return newNode(k) 
-	root = splay(root, k) 
-	# If key is already present, then return 
-	if (root.key == k): 
-		return root 
-	# If key is not present, then insert this 
-	# key into the tree 
-	# If root's key is greater, make key as 
-	# root of root's left subtree 
-	if (root.key > k): 
-		n = newNode(k) 
-		n.right = root 
-		n.left = root.left 
-		root.left = None
-		return n 
-	else: 
-		# If root's key is smaller, make key as 
-		# root of root's right subtree 
-		n = newNode(k) 
-		n.left = root 
-		n.right = root.right 
-		root.right = None
-		return n 
+            print(currPtr.data)
 
-# A utility function to print preorder 
-# traversal of the tree. 
-# The function also prints height of every 
-# node 
-def preOrder(root): 
-	if (root != None): 
-		print(root.key, end=' ') 
-		preOrder(root.left) 
-		preOrder(root.right) 
+            self.__print_helper(currPtr.left, indent, False)
+            self.__print_helper(currPtr.right, indent, True)
 
-# Driver code
-root = newNode(100)
-root.left = newNode(50)
-root.right = newNode(200)
-root.left.left = newNode(40)
-root.left.left.left = newNode(30)
-root.left.left.left.left = newNode(20)
-root = insert(root, 25)
-print("Preorder traversal of the modified Splay tree is")
-preOrder(root)
+    def __search_tree_helper(self, node, key):
+        if node == None or key == node.data:
+            return node
+
+        if key < node.data:
+            return self.__search_tree_helper(node.left, key)
+        return self.__search_tree_helper(node.right, key)
+
+    # rotate left at node x
+    def __left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left is not None:
+            y.left.parent = x
+
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+
+    # rotate right at node x
+    def __right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right is not None:
+            y.right.parent = x
+
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+
+        y.right = x
+        x.parent = y
+
+    # Splaying operation. It moves x to the root of the tree
+    def __splay(self, x):
+        while x.parent is not None:
+            if x.parent.parent is None:
+                if x == x.parent.left:
+                    # zig rotation
+                    self.__right_rotate(x.parent)
+                else:
+                    # zag rotation
+                    self.__left_rotate(x.parent)
+            elif x == x.parent.left and x.parent == x.parent.parent.left:
+                # zig-zig rotation
+                self.__right_rotate(x.parent.parent)
+                self.__right_rotate(x.parent)
+            elif x == x.parent.right and x.parent == x.parent.parent.right:
+                # zag-zag rotation
+                self.__left_rotate(x.parent.parent)
+                self.__left_rotate(x.parent)
+            elif x == x.parent.right and x.parent == x.parent.parent.left:
+                # zig-zag rotation
+                self.__left_rotate(x.parent)
+                self.__right_rotate(x.parent)
+            else:
+                # zag-zig rotation
+                self.__right_rotate(x.parent)
+                self.__left_rotate(x.parent)
+
+    # joins two trees s and t
+    def __join(self, s, t):
+        if s is None:
+            return t
+
+        if t is None:
+            return s
+
+        x = self.maximum(s)
+        self.__splay(x)
+        x.right = t
+        t.parent = x
+        return x
+
+    def __pre_order_helper(self, node):
+        if node is not None:
+            sys.stdout.write(str(node.data) + " ")
+            self.__pre_order_helper(node.left)
+            self.__pre_order_helper(node.right)
+
+    def __in_order_helper(self, node):
+        if node is not None:
+            self.__in_order_helper(node.left)
+            sys.stdout.write(str(node.data) + " ")
+            self.__in_order_helper(node.right)
+
+    def __post_order_helper(self, node):
+        if node is not None:
+            self.__post_order_helper(node.left)
+            self.__post_order_helper(node.right)
+            sys.stdout.write(str(node.data) + " ")
+
+    def preorder(self):
+        self.__pre_order_helper(self.root)
+
+    def inorder(self):
+        self.__in_order_helper(self.root)
+
+    def postorder(self):
+        self.__post_order_helper(self.root)
+
+    def search_tree(self, k):
+        x = self.__search_tree_helper(self.root, k)
+        if x is not None:
+            self.__splay(x)
+
+    def minimum(self, node):
+        if node is None:
+            return None
+        while node.left is not None:
+            node = node.left
+        return node  
+
+    def maximum(self, node):
+        if node is None:
+            return None
+        while node.right is not None:
+            node = node.right
+        return node
+    
+    def find(self, key):
+        if self.root is None:
+            return None
+        # Search for the node with the key
+        x = self.__search_tree_helper(self.root, key)
+        if x is not None:
+            self.__splay(x)
+        if self.root.data != key:
+            return None
+        return self.root.data
+    
+
+    def successor(self, x):
+        if x.right is not None:
+            return self.minimum(x.right)
+
+        y = x.parent
+        while y is not None and x == y.right:
+            x = y
+            y = y.parent
+        return y
+
+    def predecessor(self, x):
+        if x.left is not None:
+            return self.maximum(x.left)
+
+        y = x.parent
+        while y is not None and x == y.left:
+            x = y
+            y = y.parent
+        return y
+
+    def insert(self, key):
+        node =  Node(key)
+        y = None
+        x = self.root
+
+        while x is not None:
+            y = x
+            if node.data < x.data:
+                x = x.left
+            else:
+                x = x.right
+
+        node.parent = y
+        if y is None:
+            self.root = node
+        elif node.data < y.data:
+            y.left = node
+        else:
+            y.right = node
+
+        self.__splay(node)
+    
+    def remove(self, key):
+       self.search_tree(key)
+       if key != self.root.data:
+           raise ValueError('key not found in tree')
+
+       # Now delete the root.
+       if self.root.left is None:
+           self.root = self.root.right
+       else:
+           x = self.root.right
+           self.root = self.root.left
+           self.search_tree(key)
+           self.root.right = x
+
+    def pretty_print(self):
+        self.__print_helper(self.root, "", True)
+
+if __name__ == '__main__':
+    tree = SplayTree()
+    tree.insert(33)
+    tree.insert(44)
+    tree.insert(67)
+    tree.insert(5)
+    tree.insert(89)
+    tree.insert(41)
+    tree.insert(98)
+    tree.insert(1)
+    tree.pretty_print()
